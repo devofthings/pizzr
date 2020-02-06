d2629ed835c4ff60d042c5949ac3af32aa5ba1ed
Chris Winter <corvusnebula@gmail.com>
2020-02-06 18:29:12 +0100

    added two views

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import {isMobile} from 'react-device-detect';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      Hallo
-    </div>
-  );
-}
+const App = () => isMobile 
+  ?<div className="Mobile"> mobile</div>
+  :<div className="Browser">
+    
+  </div>
 
 export default App;
