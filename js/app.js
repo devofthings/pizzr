30fb6e97c3796456147f364b636034e0e023a044
Chris Winter <corvusnebula@gmail.com>
2020-02-08 19:27:31 +0100

    change card layout, fixed normal mobile view

--- js/app.js
+++ js/app.js
@@ -1,50 +1,76 @@
 // function add_button(){
-$('#btn_add').click(function() {
-    
-    var price = $('#price_input').val();
+$('#btn_add').click(function () {
+
+    const price = $('#price_input').val();
     console.log("price:", price);
 
-    if($('#radio_round').is(':checked')){
+    if ($('#radio_round').is(':checked')) {
         // round
         console.log("round pizza");
-        var diameter = $('#input_diameter').val();
+        const diameter = $('#input_diameter').val();
         console.log("diameter:", diameter);
-        var pizza_area = 3.14159 * (diameter/2)**2;
+        const pizza_area = 3.14159 * (diameter / 2) ** 2;
+        console.log("total area pizza:", pizza_area, "cm²");
+        let price_per_cm2 = price / pizza_area;
+        console.log("price per cm²:", price_per_cm2)
+        cent_per_cm2 = (price_per_cm2 * 100).toFixed(2)
+        price_per_cm2 = price_per_cm2.toFixed(4)
 
+        $('#pizza_container').append(`
+            <div class="card">
+                <img src="assets/img/pizza.png" class="card-image" alt="...">
+                <div class="card-body">
+                    <div class="input-group mb-3 card-results">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">${price}€</span>
+                            <span class="input-group-text">Ø${diameter}cm</span>
+                            <span class="input-group-text">${pizza_area}cm&sup2;</span>
+                            <span class="input-group-text">${cent_per_cm2}cent/cm&sup2;</span>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+        );
 
-    }else if($('#radio_square').is(':checked')){
+    } else if ($('#radio_square').is(':checked')) {
         // square
         console.log("square pizza");
-        var side_a = $('#input_side_a').val();
-        var side_b = $('#input_side_b').val();
+        const side_a = $('#input_side_a').val();
+        const side_b = $('#input_side_b').val();
         console.log("size:", side_a, "x", side_b);
-        var pizza_area = side_a * side_b
+        const pizza_area = side_a * side_b;
+        console.log("total area pizza:", pizza_area, "cm²");
+        let price_per_cm2 = price / pizza_area;
+        console.log("price per cm²:", price_per_cm2)
+        cent_per_cm2 = (price_per_cm2 * 100).toFixed(2)
+        price_per_cm2 = price_per_cm2.toFixed(4)
 
+        $('#pizza_container').append(`
+            <div class="card">
+                <img src="assets/img/pizza.png" class="card-image" alt="...">
+                <div class="card-body">
+                    <div class="input-group mb-3 card-results">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">${price}€</span>
+                            <span class="input-group-text">${side_a}cm X ${side_b}cm </span>
+                            <span class="input-group-text">${pizza_area}cm&sup2;</span>
+                            <span class="input-group-text">${cent_per_cm2}cent/cm&sup2;</span>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+        );
     }
-
-    console.log("total area pizza:", pizza_area, "cm²");
-    var price_per_cm2 = price / pizza_area;
-    console.log("price per cm²:", price_per_cm2)
-    cent_per_cm2 = (price_per_cm2 * 100).toFixed(2)
-    price_per_cm2 = price_per_cm2.toFixed(4)
-
-
-
-
-    $('#pizza_container').append('<div class="card"><img src="assets/img/pizza.png" width=64 height=64 alt="..."><div class="card-body"><div class="input-group mb-3"><div class="input-group-prepend"><span class="input-group-text">' + price + '€</span><span class="input-group-text">' + side_a + '.' + side_b + '</span><span class="input-group-text">' + diameter + 'Ø</span><span class="input-group-text">' + pizza_area + ' cm&sup2;</span><span class="input-group-text">' + cent_per_cm2 + 'cent/cm&sup2;</span></div><span class="input-group-text">' + price + '€</span></div></div>');
-
-
-
 });
 
 
 
-$("#radio_square").click(function(){
+$("#radio_square").click(() => {
     $("#square_div").show()
     $("#diameter_div").hide()
 });
 
-$("#radio_round").click(function(){
+$("#radio_round").click(() => {
     $("#square_div").hide()
     $("#diameter_div").show()
 });
