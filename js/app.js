f324248f42760f61c634bbb20f9c07bd135e8a8e
arv4 <github_arv4@arvid.info>
2020-02-11 00:51:44 +0100

    fixed progressbar, added highlight for best value, fixed square pizzas

--- js/app.js
+++ js/app.js
@@ -35,22 +35,36 @@ const animateCSSCurrentElement = (element, animationName, callback) => {
 // add percentage to the progressbar
 const addProgress = () => {
     let diameter = $('#input_diameter').val()
+    let side_a = $('#input_side_a').val()
+    let side_b = $('#input_side_b').val()
     let price = $('#price_input').val()
     let progress = $('#progress');
-    console.log(diameter.length)
-    console.log(price.length)
-    if(diameter.length !== 0 | price.length !== 0) {
-        progress.html("50%")
-        progress.css("width", "50%")
+    let total_percent_progress = 0;
+    if ($('#radio_round').is(':checked')) {
+        if(diameter.length !== 0) {
+            total_percent_progress += 50;
+        }
+        if(price.length !== 0) {
+            total_percent_progress += 50;
+        }
+    }else if($('#radio_square').is(':checked')) {
+        if(side_a.length !== 0) {
+            total_percent_progress += 33;
+        }
+        if(side_b.length !== 0) {
+            total_percent_progress +=33;
+        }
+        if(price.length !== 0) {
+            total_percent_progress += 34;
+        }
     }
-    if(diameter.length !== 0 & price.length !== 0) {
-        progress.html("100%")
-        progress.css("width", "100%")
+
+    progress.html(total_percent_progress+"%")
+    progress.css("width", total_percent_progress+"%")
+    if (total_percent_progress == 100){
         $('#btn_add').removeClass('disabled');
-    }
-    if(diameter.length == 0 & price.length == 0) {
-        progress.html("0%")
-        progress.css("width", "10%")
+    }else if(total_percent_progress == 0){
+        progress.css("width", "10%");
     }
 }
 
@@ -58,33 +72,61 @@ const addProgress = () => {
 const getPurged = () => {
     $('#input_diameter').val(null)
     $('#price_input').val(null)
+    $('#input_side_a').val(null)
+    $('#input_side_b').val(null)
     $('#btn_add').addClass('disabled');
 } 
+
+const highlight_best_value = () => {
+    all_price_per_cm2 = $('.price_per_cm2');
+    all_price_per_cm2.parent().removeClass('best-value');
+    let best_price = all_price_per_cm2[0];
+
+    all_price_per_cm2.each(function(){
+        if(this.innerHTML < best_price.innerHTML){
+            best_price = this;
+        }
+    })
+
+    best_price.parentNode.classList.add("best-value");
+}
+
 
 
 // MAIN FUNCTIONS
 // switch input-form on radio selection
 $("#radio_square").click(() => {
-    $("#square_div").show()
-    $("#diameter_div").hide()
+    $("#diameter_div").hide();
+    $("#square_div").show();
+    getPurged();
+    addProgress();
 });
 
 $("#radio_round").click(() => {
-    $("#square_div").hide()
-    $("#diameter_div").show()
+    $("#square_div").hide();
+    $("#diameter_div").show();
+    getPurged();
+    addProgress();
 });
 
 // check for input in the input fields
+$('#input_side_a').keyup(() => {
+    addProgress()
+})
+$('#input_side_b').keyup(() => {
+    addProgress()
+})
 $('#input_diameter').keyup(() => {
     addProgress()
 })
-
 $('#price_input').keyup(() => {
     addProgress()
 })
+
 // create new cards
 $('#btn_add').click(() => {
-    if ($('#input_diameter').val().length === 0 || $('#price_input').val().length === 0){
+    
+    if ($('#btn_add').hasClass('disabled')){
         alert('One or Two fields are empty. Please fill up all fields');
         return null;
     }
@@ -117,7 +159,7 @@ $('#btn_add').click(() => {
                             <span class="input-group-text input-group-card">${price}€</span>
                             <span class="input-group-text input-group-card">Ø${diameter}cm</span>
                             <span class="input-group-text input-group-card">${pizza_area}cm&sup2;</span>
-                            <span class="input-group-text input-group-card">${cent_per_cm2}cent/cm&sup2;</span>
+                            <span class="input-group-text input-group-card"><span class="price_per_cm2">${cent_per_cm2}</span>cent/cm&sup2;</span>
                         </div>
                     </div>
                 </div>
@@ -140,21 +182,25 @@ $('#btn_add').click(() => {
 
         $('#pizza_container').append(`
             <div class="card">
+                <div id="close-button" onClick="deleteCard($(this))">
+                    <span class="text-center text-light font-weight-bold close-button-x">x</span>
+                </div>
                 <img src=${getPizzaImageString()} class="card-image" alt="...">
                 <div class="card-body">
                     <div class="input-group mb-3 card-results">
                         <div class="input-group-prepend">
-                            <span class="input-group-text">${price}€</span>
-                            <span class="input-group-text">${side_a}cm X ${side_b}cm </span>
-                            <span class="input-group-text">${pizza_area}cm&sup2;</span>
-                            <span class="input-group-text">${cent_per_cm2}cent/cm&sup2;</span>
+                            <span class="input-group-text input-group-card">${price}€</span>
+                            <span class="input-group-text input-group-card">${side_a}cm X ${side_b}cm </span>
+                            <span class="input-group-text input-group-card">${pizza_area}cm&sup2;</span>
+                            <span class="input-group-text input-group-card"><span class="price_per_cm2">${cent_per_cm2}</span>cent/cm&sup2;</span>
                         </div>
                     </div>
                 </div>
             </div>`
         );
     }
-    getPurged()
+    getPurged();
+    highlight_best_value();
 });
 
 //delete card
